28956789734700573cd97da169a06cc7217841fe
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-10 00:34:20 -0300

    adicionado a conexão com o banco de dados

---
+++ grupos_estudo/database.py
@@ -0,0 +1,14 @@
+from neo4j import GraphDatabase
+
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "neo4j://localhost:7687"
+AUTH = ("neo4j", "6grKSWQvHSXtwpqWWegu")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    try:
+        driver.verify_connectivity()
+        print("conectado com sucesso")
+    except Exception:
+        print("conexão falhou")
+        
+        
