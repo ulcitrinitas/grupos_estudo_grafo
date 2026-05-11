368e555011e24799239a9b7177dfd12807d27643
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-11 16:54:30 -0300

    adicionado insert no banco de dados

--- grupos_estudo/main.py
+++ grupos_estudo/main.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-from database import testa_conn
+from database import testa_conn, insert_query
 from person import Person_Data
 
 db_auth = testa_conn()
@@ -8,3 +8,5 @@ db_auth = testa_conn()
 p = Person_Data.model_validate({"name": "Alice", "friendname": "Bob"})
 
 pprint(p)
+
+insert_query(p, db_auth)
