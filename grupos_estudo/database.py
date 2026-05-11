fd5d8037c1b43323bb4763dfb24e6d1ff6e38119
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-11 16:10:49 -0300

    alterado try/except

--- grupos_estudo/database.py
+++ grupos_estudo/database.py
@@ -1,26 +1,13 @@
-from neo4j import GraphDatabase
-
-# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-URI = "neo4j://localhost:7687"
-AUTH = ("neo4j", "6grKSWQvHSXtwpqWWegu")
-
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    try:
-        driver.verify_connectivity()
-        print("conectado com sucesso")
-    except:
-        print("conexão falhou")
-
 def testa_conn():
     from neo4j import GraphDatabase
 
-# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-URI = "neo4j://localhost:7687"
-AUTH = ("neo4j", "6grKSWQvHSXtwpqWWegu")
+    # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+    URI = "neo4j://localhost:7687"
+    AUTH = ("neo4j", "6grKSWQvHSXtwpqWWegu")
 
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    try:
-        driver.verify_connectivity()
-        print("conectado com sucesso")
-    except:
-        print("conexão falhou")
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        try:
+            driver.verify_connectivity()
+            print("conectado com sucesso")
+        except Exception:
+            print("conexão falhou")
