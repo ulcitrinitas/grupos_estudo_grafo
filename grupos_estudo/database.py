771a6a79aa6a95dc66652bd8bb9b50fc0387b947
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-11 16:08:42 -0300

    criada função para o teste da conecção

--- grupos_estudo/database.py
+++ grupos_estudo/database.py
@@ -8,7 +8,19 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
     try:
         driver.verify_connectivity()
         print("conectado com sucesso")
-    except Exception:
+    except:
         print("conexão falhou")
-        
-        
+
+def testa_conn():
+    from neo4j import GraphDatabase
+
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "neo4j://localhost:7687"
+AUTH = ("neo4j", "6grKSWQvHSXtwpqWWegu")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    try:
+        driver.verify_connectivity()
+        print("conectado com sucesso")
+    except:
+        print("conexão falhou")
