60925cb65721fe28a17431bb7cd391ca23212805
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-11 18:23:00 -0300

    adicionado decorações nos prints

--- grupos_estudo/main.py
+++ grupos_estudo/main.py
@@ -1,12 +1,14 @@
 from pprint import pprint
 
-from database import testa_conn, insert_query
-from grupos_estudo.classes.person import Person_Data
+from database import testa_conn, insert_query, query_graph
+from classes.person import Person_Data
 
 db_auth = testa_conn()
 
 p = Person_Data.model_validate({"name": "Alice", "friendname": "Bob"})
 
 pprint(p)
-
+print("--------------------------------------------------------------------------")
 insert_query(p, db_auth)
+print("--------------------------------------------------------------------------")
+query_graph(db_auth)
