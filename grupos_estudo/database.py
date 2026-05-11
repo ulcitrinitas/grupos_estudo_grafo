e80beb60efb711cfb9c6ddc5aa3fd09bded123d8
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-11 18:31:10 -0300

    adicionados imports das classes

--- grupos_estudo/database.py
+++ grupos_estudo/database.py
@@ -1,7 +1,8 @@
 from neo4j import GraphDatabase
 
-from grupos_estudo.classes.person import Person_Data
-
+from classes.aluno import Aluno
+from classes.curso import Curso
+from classes.grupo_estudo import Grupo_Estudo
 
 def testa_conn():
     # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
@@ -21,16 +22,19 @@ def testa_conn():
 def insert_query(data: Person_Data, db_auth):
     with GraphDatabase.driver(db_auth["uri"], auth=db_auth["auth"]) as driver:
         try:
-            summary = driver.execute_query("""
+            summary = driver.execute_query(
+                """
                     CREATE (a:Person {name: $name})
                     CREATE (b:Person {name: $friendName})
                     CREATE (a)-[:KNOWS]->(b)
             """,
-            name=data.name, friendName=data.friendname,
-            database_= "neo4j").summary
-            
+                name=data.name,
+                friendName=data.friendname,
+                database_="neo4j",
+            ).summary
+
             print(f"CREATED ({data.name})-[:KNOWS]->({data.friendname})")
-            
+
             print(
                 "Created {nodes_created} nodes in {time} ms.".format(
                     nodes_created=summary.counters.nodes_created,
@@ -39,14 +43,15 @@ def insert_query(data: Person_Data, db_auth):
             )
         except Exception:
             print("Erro ao inserir os dados")
-            
+
 
 def query_graph(db_auth):
     with GraphDatabase.driver(db_auth["uri"], auth=db_auth["auth"]) as driver:
         try:
-            records, summary, keys = driver.execute_query("""
-                MATCH (p:Person)-[:KNOWS]->(:Person)
-                RETURN p.name AS name
+            records, summary, keys = driver.execute_query(
+                """
+                MATCH (a:Aluno)-[:PARTICIPA_DE]->(g:GrupoEstudo)
+                RETURN a.nome, collect(g.nome) AS grupos
                 """,
                 database_="neo4j",
             )
@@ -55,9 +60,12 @@ def query_graph(db_auth):
                 print(record.data())  # obtain record as dict
 
             # Summary information
-            print("The query `{query}` returned {records_count} records in {time} ms.".format(
-                query=summary.query, records_count=len(records),
-                time=summary.result_available_after
-            ))
+            print(
+                "The query `{query}` returned {records_count} records in {time} ms.".format(
+                    query=summary.query,
+                    records_count=len(records),
+                    time=summary.result_available_after,
+                )
+            )
         except Exception:
             print("Erro ao pegar o grafo")
