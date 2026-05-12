12d7a02d284383416096b1bd5fe530bcea96c939
Ulisses SC Silva <ulisses.scsilva@proton.me>
2026-05-11 22:30:19 -0300

    alterado para usar as dataclasses

--- grupos_estudo/main.py
+++ grupos_estudo/main.py
@@ -1,14 +1,26 @@
 from pprint import pprint
 
-from database import testa_conn, insert_query, query_graph
-from classes.person import Person_Data
+from database import testa_conn, insert_aluno_curso, query_aluno_curso_graph
+from classes.aluno import Aluno
+from classes.curso import Curso
 
 db_auth = testa_conn()
 
-p = Person_Data.model_validate({"name": "Alice", "friendname": "Bob"})
+a = Aluno.model_validate(
+    {
+        "nome": "Ana Pereira",
+        "matricula": 74128,
+        "email": "ana_pereira@email.com",
+        "idade": 24,
+    }
+)
 
-pprint(p)
-print("--------------------------------------------------------------------------")
-insert_query(p, db_auth)
-print("--------------------------------------------------------------------------")
-query_graph(db_auth)
+c = Curso.model_validate({"nome": "Inteligência Artificial", "duraçao": 8})
+
+print("---------------------------------------------------")
+pprint(a)
+pprint(c)
+print("---------------------------------------------------")
+insert_aluno_curso(a, c, db_auth)
+print("---------------------------------------------------")
+query_aluno_curso_graph(db_auth)
